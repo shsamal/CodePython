85ae652575db988ce75e98ddde932126fa2fab48
shsamal <132987049+shsamal@users.noreply.github.com>
2024-01-06 23:10:21 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,15 @@
+# Write a function called "show_excitement" where the string
+# "I am super excited for this course!" is returned exactly
+# 5 times, where each sentence is separated by a single space.
+# Return the string with "return".
+# You can only have the string once in your code.
+# Don't just copy/paste it 5 times into a single variable!
+
+
+def show_excitement():
+    i = "I am super excited for this course!"
+    return (i + " ") * 5
+    # Your code goes here!
+
+
+print(show_excitement())
